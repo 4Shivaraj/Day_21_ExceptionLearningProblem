3a32a8c27b6e5402edfbaad5c2b11bd21104af40
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-09 01:01:46 +0530

    [Shivaraj] Added Index Out Of Range Exception

---
+++ ExceptionLearningProblem/ExceptionLearningProblem/Exceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionLearningProblem
+{
+    class TypeOfExceptions
+    {
+        public static void TestIndexOutOfRangeException()
+        {
+            try
+            {
+                int[] arr = { 10, 20, 30, 40};
+                Console.WriteLine(arr[4]);
+            }
+            catch(IndexOutOfRangeException ex)  //Default Exception
+            {
+                Console.WriteLine(ex);
+            }
+            catch(Exception ex)                 //Parent Exception
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
--- ExceptionLearningProblem/ExceptionLearningProblem/Program.cs
+++ ExceptionLearningProblem/ExceptionLearningProblem/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Exception Learning Problem");
+
+namespace ExceptionLearningProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Exception Learning Problem");
+            TypeOfExceptions.TestIndexOutOfRangeException();
+            Console.ReadLine();
+        }
+    }
+}
