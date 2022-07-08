2d72cd5469d47fed6cceb368833310dc854f1fba
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-09 01:46:44 +0530

    [Shivaraj] Added Handling Other Type Of Exception

--- ExceptionLearningProblem/ExceptionLearningProblem/Exceptions.cs
+++ ExceptionLearningProblem/ExceptionLearningProblem/Exceptions.cs
@@ -12,14 +12,14 @@ namespace ExceptionLearningProblem
         {
             try
             {
-                int[] arr = { 10, 20, 30, 40};
+                int[] arr = { 10, 20, 30, 40 };
                 Console.WriteLine(arr[4]);
             }
-            catch(IndexOutOfRangeException ex)  //Default Exception
+            catch (IndexOutOfRangeException ex)  //Default Exception
             {
                 Console.WriteLine(ex);
             }
-            catch(Exception ex)                 //Parent Exception
+            catch (Exception ex)                 //Parent Exception
             {
                 Console.WriteLine(ex);
             }
@@ -31,11 +31,46 @@ namespace ExceptionLearningProblem
                 string name = null;
                 int count = name.Length;
             }
-            catch (NullReferenceException ex)  //Default Exception
+            catch (NullReferenceException ex)   //Default Exception
             {
                 Console.WriteLine(ex);
             }
             catch (Exception ex)                 //Parent Exception
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public static void TestArithmeticException()
+        {
+            try
+            {
+                int a = 10;
+                int c = a / 0;
+            }
+            catch (ArithmeticException ex)          //Default Exception
+            {
+                Console.WriteLine(ex);
+            }
+            catch (Exception ex)                    //Parent Exception
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public static void TestArgumentoutOfRangeException()
+        {
+            try
+            {
+                var numbers = new List<int>();
+                var index = 0;
+                Console.WriteLine("Removing item at index {0}", index);
+
+                numbers.RemoveAt(index);
+            }
+            catch (ArgumentOutOfRangeException ex)      //Default Exception
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)                        //Parent Exception
             {
                 Console.WriteLine(ex);
             }
--- ExceptionLearningProblem/ExceptionLearningProblem/Program.cs
+++ ExceptionLearningProblem/ExceptionLearningProblem/Program.cs
@@ -4,11 +4,15 @@ namespace ExceptionLearningProblem
 {
     class Program
     {
+        public static string Shivaraj { get; private set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Exception Learning Problem");
             TypeOfExceptions.TestIndexOutOfRangeException();
             TypeOfExceptions.TestNullReferenceException();
+            TypeOfExceptions.TestArithmeticException();
+            TypeOfExceptions.TestArgumentoutOfRangeException();
             Console.ReadLine();
         }
     }
